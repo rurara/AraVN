ff3fffaee25a6c63dfca924f196a13cc61e9cf13
rurara <kgsin7@gmail.com>
2012-08-02 14:25:15 +0900

    gsgsdf

---
+++ AraVN/ChapterSelectButtonViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChapterSelectButtonViewController.h
+//  AraVN
+//
+//  Created by happy rurara on 12. 8. 2..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChapterSelectButtonViewController : UIButton
+
+@end
---
+++ AraVN/ChapterSelectButtonViewController.m
@@ -0,0 +1,31 @@
+//
+//  ChapterSelectButtonViewController.m
+//  AraVN
+//
+//  Created by happy rurara on 12. 8. 2..
+//
+//
+
+#import "ChapterSelectButtonViewController.h"
+
+@implementation ChapterSelectButtonViewController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
