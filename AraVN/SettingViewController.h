bdaf39002dcabda9196c6ddfa19efad65af6f66d
rurara <kgsin7@gmail.com>
2012-08-02 11:35:05 +0900

    ignore

---
+++ AraVN/SettingViewController.h
@@ -0,0 +1,15 @@
+//
+//  SettingViewController.h
+//  cyphersGame01
+//
+//  Created by happy rurara on 12. 7. 28..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+- (IBAction)saveButtonAction:(id)sender;
+- (IBAction)loadBUttonAction:(id)sender;
+
+@end
---
+++ AraVN/SettingViewController.m
@@ -0,0 +1,59 @@
+//
+//  SettingViewController.m
+//  cyphersGame01
+//
+//  Created by happy rurara on 12. 7. 28..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import "SettingViewController.h"
+
+@interface SettingViewController ()
+
+@end
+
+@implementation SettingViewController
+
+
+- (IBAction)loadBUttonAction:(id)sender {
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+- (IBAction)saveButtonAction:(id)sender {
+    [self dismissModalViewControllerAnimated:YES];
+}
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)dealloc {
+
+    [super dealloc];
+}
+@end
